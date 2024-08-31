82d375af8d1d3dab9ce22637a67a994e50cd7939
Zach Moolman <jzmmoolman@scapticom.co.za>
2024-08-30 20:00:23 -0600

    Test framework - initial config

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,74 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
 
 use core::fmt::Write;
 use core::panic::PanicInfo;
 
 mod vga_buffer;
+mod serial;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+// our existing panic handler
+#[cfg(not(test))] // new attribute
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+// our panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    println!("Hello World{}", "!");
-    println!("Hello World{}", "!");
-    println!("Hello World{}", "!");
-    println!("Hello World{}", "!");
-    print!("Hello World{}", "!");
-    println!();
-    print!("Hello World{}", "!");
-    println!();
-    print!("Hello World");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    // println!("Running {} tests", tests.len());
+    serial_println!("Running {} test", tests.len());
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    //print!("trivial assertion... ");
+    serial_println!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
