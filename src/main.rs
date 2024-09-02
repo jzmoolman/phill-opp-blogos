2c668a6cf138e8922a33d91224ba4e435512f663
Zach Moolman <jzmmoolman@scapticom.co.za>
2024-09-02 13:10:16 -0600

    Use crate to to translation

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,19 @@
 
 use core::panic::PanicInfo;
 use bootloader::{entry_point, BootInfo};
-use blog_os::println;
+use x86_64::structures::paging::Translate;
+use blog_os::{memory, println};
 
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    // use blog_os::memory::active_level_4_table;
     use x86_64::VirtAddr;
-    use x86_64::structures::paging::PageTable;
-    use blog_os::memory::translate_addr;
 
     println!("Hello World{}", "!");
     blog_os::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mapper = unsafe { memory::init(phys_mem_offset) };
 
     let addresses = [
         // the identity-mapped vga buffer page
@@ -34,17 +33,17 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     for &address in &addresses {
         let virt = VirtAddr::new(address);
-        let pyhys = unsafe { translate_addr(virt, phys_mem_offset) };
-        println!("{:?} ->  {:?}", virt, pyhys);
+        let phys = mapper.translate_addr(virt);
+        println!("{:?} ->  {:?}", virt, phys);
     }
 
 
 
-    fn stack_overflow() {
-        stack_overflow(); // for ech recursion, the return address is pushed
-    }
-
-    stack_overflow();
+    // fn stack_overflow() {
+    //     stack_overflow(); // for ech recursion, the return address is pushed
+    // }
+    //
+    // stack_overflow();
 
     #[cfg(test)]
     test_main();
